d1b3ef07ec1dd66308b7af1cdd201b157b77be12
Nishant Rajpoot <nishant.rajpoot@xcdify.com>
2023-12-11 15:31:41 +0530

    cred navigate

--- src/pages/credential.tsx
+++ src/pages/credential.tsx
@@ -1,10 +1,16 @@
 import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { LoginContext } from "../App";
 import { CredData } from "../data/credential-data";
 
 function Credential() {
+  const navigate = useNavigate();
   const { authContext } = useContext(LoginContext);
   const user = authContext.userDetail;
+
+  const navigateToUrl = (url: string) => {
+    navigate(url);
+  };
 
   return (
     <div className="p-2 bg-white mt-[1.313rem] rounded-t-[1rem]">
@@ -12,7 +18,8 @@ function Credential() {
         {CredData(user).map((item, i) => (
           <div
             key={i}
-            className="flex items-center justify-between px-4 mt-[24px] w-[90vw] h-[4.5rem] md:w-[62rem] rounded-[0.5rem] m-auto bg-[#F5F6FA] border-[1px] border-[#EBECF1]"
+            onClick={() => navigateToUrl(item.url)}
+            className="flex items-center justify-between px-4 mt-[24px] w-[90vw] h-[4.5rem] md:w-[62rem] rounded-[0.5rem] m-auto bg-[#F5F6FA] border-[1px] border-[#EBECF1] cursor-pointer"
           >
             <div className="flex flex-row items-center gap-2">
               <img src={item.icon} alt="icon" />
